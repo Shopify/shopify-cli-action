370ae3d18bdc8448a92db8a292b2a365b57a0898
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2021-12-23 12:57:24 +0100

    Fix linter

--- src/input.js
+++ src/input.js
@@ -2,4 +2,5 @@ const core = require('@actions/core');
 
 exports.command = () => core.getInput('command', { required: true });
 exports.path = () => core.getInput('path', { required: false });
-exports.authToken = () => core.getInput('auth-token', { required: false });
+exports.authToken = () =>
+  core.getInput('auth-token', { required: false });
