ae6874c2794bfa5665036daa26660d315bc407a7
Pedro Piñera <pedro@craftweg.com>
2022-01-05 10:29:11 +0100

    Resolve the path

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const core = require('@actions/core');
+const path = require('path');
 const execSync = require('./execSync');
 const input = require('./input');
 
 module.exports = () => {
   try {
     execSync('gem install shopify-cli');
-    const path = input.path() || '.';
+    let cwd;
+    if (input.path()) {
+      cwd = path.resolve(input.path());
+    } else {
+      cwd = process.cwd();
+    }
     const env = {};
     if (input.authToken()) {
       env.SHOPIFY_CLI_AUTH_TOKEN = input.authToken();
     }
-    execSync(`shopify ${input.command()}`, { cwd: path }, env);
+    execSync(`shopify ${input.command()}`, { cwd }, env);
   } catch (error) {
     core.setFailed(error.message);
   }
